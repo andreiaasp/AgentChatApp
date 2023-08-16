1ccc2cde54673a35b44e39e2b02c5512af9c512d
Andreia <alexandraper12@hotmail.com>
2023-08-16 23:23:17 +0100

    Frontend: Added Messages timestamp on ChatBox

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -4,26 +4,61 @@ import './ChatWindow.css';
 function ChatWindow({ messages }) {
     
     const messageContainerRef = useRef(null);
+    const messagesByDay = {};
+
 
     useEffect(() => {
         scrollToBottom();
     }, [messages]);
 
+    //scroll the scrollbar to the bottom
     const scrollToBottom = () => {
         if (messageContainerRef.current) {
             messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
         }
     };
 
+    //format timestamp of the message in time only
+    function formatTimestamp(timestamp) {
+        return new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    }
+
+    function formatDate(timestamp) {
+        return new Date(timestamp).toLocaleDateString([], {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+    }
+
+    messages.forEach((message) => {
+        const date = formatDate(message.timestamp);
+        if (!messagesByDay[date]) {
+            messagesByDay[date] = [];
+        }
+        messagesByDay[date].push(message);
+    });
+
     return (
     <div  ref={messageContainerRef} className="message-container-wrapper">
         <div className="message-container">
-            {messages.map((message, index) => (
-            <div
-                key={index}
-                className={message.isUser ? 'outgoing-bubble' : 'incoming-bubble'}>
-                {message.content}
-            </div>
+            {Object.entries(messagesByDay).map(([date, messages]) => (
+                <div key={date} className="message-group">
+                    <div className="date">{date}</div>
+                    {messages.map((message, index) => (
+                        <div key={index} className={`message-info ${message.isUser ? 'align-right' : 'align-left'}`}>
+                            <div style={{ margin: '4px'}}>
+                                {message.isUser ? 'Me' : 'Bot'}
+                            </div>
+                            <div className={message.isUser ? 'outgoing-bubble' : 'incoming-bubble'}>
+                                {message.content}
+                            </div>
+                            <div className={`timestamp ${message.isUser ? 'align-right' : 'align-left'}`}>
+                                {formatTimestamp(message.timestamp)}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             ))}
         </div>
     </div>
